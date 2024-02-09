373e04e0b163740c9234aefae0dcbf338c5a56d3
dev <dev@vscode.blend>
2024-02-09 14:54:19 -0300

    chore: factory structural base app

--- src/utils/types/index.ts
+++ src/utils/types/index.ts
@@ -1,12 +1,14 @@
-import { HTMType, TState } from 'iares';
+import { HTMType, TState } from "iares";
 
-export type TemplateType<T extends Object> = { (params: T): HTMType | HTMType[]}
-export type PropsType<T extends Object> = T
-export type ParamsType<T> = { params: T}
-export type ActionsType<T extends Object> = T
-export type HooksType<T extends Object> = T
+export interface Props extends Object {}
+export type TemplateType<T extends Props> = {
+  (params: T): HTMType | HTMType[];
+};
+export type PropsType<T extends Props> = T;
+export type ParamsType<T> = { params: T };
+export type ActionsType<T extends Props> = T;
+export type HooksType<T extends Props> = T;
 
-export type TemplateParamsType<T extends Object> = T
-export type TemplateStateType<T extends Object> = T
-export type StateType<T extends Object> = TState<T>
-
+export type TemplateParamsType<T extends Props> = T;
+export type TemplateStateType<T extends Props> = T;
+export type StateType<T extends Props> = TState<T>;
